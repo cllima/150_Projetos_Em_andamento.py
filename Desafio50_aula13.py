c4508807808766a7ae165fbab12b3edd3298743e
clayton lima <cllima36@hotmail.com>
2023-01-08 09:00:56 -0300

    Initial commit

--- Desafio50_aula13.py
+++ Desafio50_aula13.py
@@ -1,4 +1,10 @@
 # Desenvolva um programa que leia 6 números inteiros e mostre a soma apenas daqueles que forem pares.
 # Se o valor digitado for ímpar desconsidere-o.
 
-
+soma = 0
+cont = 0
+for c in range(1,7):
+    num = int(input('Digite 0 {} valor: '.format(c)))
+    soma += num
+    cont += 1
+print('Foi informado {} números e a soma é {}.'.format(cont,soma))
