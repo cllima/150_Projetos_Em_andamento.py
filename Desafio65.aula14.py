d2d1f76e7f84f63ba3113e260858ef7c9818b9eb
clayton lima <93720674+cllima@users.noreply.github.com>
2023-01-16 12:01:08 -0300

    Delete Desafio65.aula14.py
    duplicidade

--- Desafio65.aula14.py
+++
@@ -1,3 +0,0 @@
-## Crie um programa que leia vários números inteiros pelo teclado. No final da execução,
-# mostre a média entre todos os valores e qual foi o MAIOR e o MENOR valores lidos.
-# O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
