6fa9840dc983e5c0ea83e3d52347871ee09fe4da
clayton lima <93720674+cllima@users.noreply.github.com>
2023-01-24 09:37:23 -0300

    Delete Desafio73.aula16.py
    Duplicidade

--- Desafio73.aula16.py
+++
@@ -1,7 +0,0 @@
-## Crie uma Tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol,
-## na ordem de colocação. Depois mostre:
-#A) Os 5 primeiros colocados.
-#B) OS últimos 4 colocados da Tabela.
-#C) Uma lista com os Times em ordem alfabética.
-#D) Em que posição está o Time da Chapecoense.,
-# OBS.: Pegar a Tabela do Campeonato np Google.
