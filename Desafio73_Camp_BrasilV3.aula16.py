5310868cb8168dedf8639fc3e791b203a58564a2
clayton lima <cllima36@hotmail.com>
2023-01-24 10:03:59 -0300

    Initial commit

--- Desafio73_Camp_BrasilV3.aula16.py
+++ Desafio73_Camp_BrasilV3.aula16.py
@@ -1,10 +1,6 @@
 ## Crie uma Tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol,
 ## na ordem de colocação. Depois mostre:
-#A) Os 5 primeiros colocados.
-#B) OS últimos 4 colocados da Tabela.
-#C) Uma lista com os Times em ordem alfabética.
-#D) Em que posição está o Time da Chapecoense.,
-# OBS.: Pegar a Tabela do Campeonato np Google.
+#A) Mostre conforme solicitado abaixo.
 
 from time import sleep
 times = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo',
