6e23a65c9f60ed51ecde82582ba5ff5413043b12
clayton lima <cllima36@hotmail.com>
2023-01-25 08:05:15 -0300

    Initial commit

--- Desafio73_Camp_BrasilV3.aula16.py
+++ Desafio73_Camp_BrasilV3.aula16.py
@@ -1,18 +1,23 @@
 ## Crie uma Tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol,
-## na ordem de colocação. Depois mostre:
+## na ordem de colocação. Depois:
 #A) Mostre conforme solicitado abaixo.
 
 from time import sleep
 times = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo',
-             'Athético-PR', 'Atlético-MG', 'Fortaleza', 'São Paulo', 'América-MG', 'Botafogo',
-             'Santos', 'Goiás', 'Bragantino', 'Cuiabá', 'Ceará', 'Atlético-GO', 'Avaí', 'Juventude', 'Coritiba')
+             'Athético-PR', 'Atlético-MG', 'Fortaleza', 'São Paulo', 'América-MG',
+	     'Botafogo','Santos', 'Goiás', 'Bragantino', 'Cuiabá', 'Ceará',
+	     'Atlético-GO', 'Avaí', 'Juventude', 'Coritiba')
+
 print("="*50)
 print("{:^50}".format("DADOS DO BRASILEIRÃO 2022"))
 print("="*50)
+
 opcao = 0
 while opcao != 8:
     sleep(1.2)
-    opcao = int(input('''[1] O Campeão do campeonato
+
+    opcao = int(input('''
+[1] O Campeão do Brasileirão 2022
 [2] Classificados para fase de grupos da Libertadores 
 [3] Classificados para Pré - Libertadores 
 [4] Classificados para fase de grupos da Copa Sul-Americana
@@ -22,18 +27,20 @@ while opcao != 8:
 [8] Sair do programa do brasileirão
 Digite uma Opção: '''))
     print("="*50)
+
     if opcao == 1:
-        print("O campeão foi o {}".format(times[0])) # Primeiro colocado
+        print(f"O Time campeão foi o {times[0]}!!") # Primeiro colocado
     elif opcao == 2:
-        print(f"Classificados para Libertadores 2023: {times[:6]}") # Os 6 primeiros
+        print(f"6 TIMES estão classificados para Libertadores 2023: \n{times[:6]}") # Os 6 primeiros
     elif opcao == 3:
-        print(f"Classificados para Pré - Libertadores 2023: {times[6:8]}") # 2 times
+        print(f"2 TIMES estão classificados para Pré - Libertadores 2023: \n{times[6:8]}") # 2 times
     elif opcao == 4:
-        print(f"Classificados para Sul-Americana 2023: {times[8:14]}") #6 times
+        print(f"6 TIMES estão classificados para Sul-Americana 2023: \n{times[8:14]}") # 6 times
     elif opcao == 5:
-        print(f"Rebaixados para Segunda Divisão: {times[-4:]}") # Os 4 últimos times
+        print(f"4 TIMES estão rebaixados para Segunda Divisão: \n{times[-4:]}") # Os 4 últimos times
     elif opcao == 6:
-        time = str(input("Escreva o nome do seu time: ")).strip().title() # Obter a posição do Time.
+        time = str(input("Escreva o nome do seu TIME: ")).strip().title() # Obter a posição do Time.
+
         if time in times:
             print(f"O {time} ficou na {times.index(time)+1}º Posição")
             print("Situação: ",end="")
@@ -46,18 +53,20 @@ Digite uma Opção: '''))
             elif times.index(time) >= 8 and times.index(time) <= 13:
                 print("Está na Copa Sul-Americana")
             elif times.index(time) >= 16:
-                print("Está Rebaixado para a Serie B")
+                print("Seu TIME está Rebaixado para a Serie B!!!")
             else:
-                print("Ficou na Serie A, porém não se classificou para nenhuma outra competição")
+                print("Ficou na Serie A, porém não se classificou para nenhuma outra competição!!")
         else:
-            print("O {} não participou do Brasileirão 2022".format(time))
+            print(f"O {time} não participou do Brasileirão 2022")
+
     elif opcao == 7:
         cont = 0
         print("="*50)
         print("{:^50}".format("TABELA DO BRASILEIRÃO 2022"))
         print("="*50)
+
         for tabela in times:
             print(f"{cont + 1}º {tabela}")
             cont += 1
     print("=" * 50)
-print("Fim do programa do campeonato brasileiro")
+print("Fim do Programa do Campeonato Brasileiro")
