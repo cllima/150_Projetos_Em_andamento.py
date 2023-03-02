7f14f3b47378f1829e212b7bb28a751fa511ae55
clayton lima <cllima36@hotmail.com>
2023-03-02 08:47:56 -0300

    Initial commit

---
+++ Desafio105_AnalisandoegerandoDicionários.Aula21.py
@@ -0,0 +1,38 @@
+#Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai
+# retornar um dicionário com as seguintes informações:
+# Quantidade de notas
+# A maior nota
+# A menor nota
+# A média da turma
+# A situação (opcional)
+# Adicione tbm a ocstrings
+
+print('Analisando e gerando Dicionários'.center(35).upper())
+print('='*35)
+
+def notas(*n, sit=False):
+    """
+    -> Função para analisar Notas e situações de vários alunos.
+    :param n: Uma ou mais notas dos alunos (aceita várias)
+    :param sit: Valor opcional indicando se deve ou não adicionar a situação.
+    :return: Dicionário com várias informações sobre a situação da turma.
+    """
+    r=dict()
+    r['total']=len(n)
+    r['maior']=max(n)
+    r['menor']=min(n)
+    r['média']=sum(n)/len(n)
+    if sit:
+        if r['média'] >= 7:
+            r['situação']= 'BOA'
+        elif r['média']>=5:
+            r['situação']= 'RAZOÁVEL'
+        else:
+            r['situação']= 'RUIM'
+
+    return r
+
+## Programa Principal
+resp=notas(5.5,7.5,9.,8.5, sit=True)
+print(resp)
+#help(notas)
